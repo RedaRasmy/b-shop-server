f3b9c275806c8c68f01641b2d98f36ae9422d258
redarasmy <red7mer@gmail.com>
2025-09-26 15:41:08 +0100

    protect add/update/delete products

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -9,19 +9,30 @@ import {
 import { validateBody, validateIdParam } from '../lib/validator-functions'
 import { insertFullProductSchema } from '../db/zod-schemas'
 import { upload } from '../lib/upload'
+import { requireAuth } from '../middlewares/require-auth'
 
 const router: Router = Router()
 
+// public 
 router.get('/', getProducts)
 router.get('/:id', validateIdParam(), getProductById)
-router.post('/', upload.array('images',5) ,validateBody(insertFullProductSchema), addProduct)
+
+// protected
+router.post(
+  '/',
+  requireAuth('admin'),
+  upload.array('images', 5),
+  validateBody(insertFullProductSchema),
+  addProduct,
+)
 router.put(
   '/:id',
-  upload.array('images',5),
+  requireAuth('admin'),
+  upload.array('images', 5),
   validateIdParam(),
   validateBody(insertFullProductSchema),
   updateProduct,
 )
-router.delete('/:id', validateIdParam(), deleteProduct)
+router.delete('/:id', requireAuth('admin'), validateIdParam(), deleteProduct)
 
 export default router
