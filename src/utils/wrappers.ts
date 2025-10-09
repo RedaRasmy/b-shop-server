11bfe74a36e51b0d3b7258ba7fd73ce75e9761a8
redarasmy <red7mer@gmail.com>
2025-10-09 19:45:26 +0100

    add wrapper for each type of request

---
+++ src/utils/wrappers.ts
@@ -0,0 +1,122 @@
+import type { NextFunction, Request, Response } from 'express'
+import { z } from 'zod'
+const IdSchema = z.string().min(1)
+
+export function makeGetEndpoint<Query>(
+  schema: z.ZodType<Query>,
+  callback: (
+    req: Request<any, any, any, Query>,
+    res: Response,
+    next: NextFunction,
+  ) => void,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.query)
+
+    if (!result.success) {
+      return res.status(400).send({
+        message: 'Invalid query params',
+        details: result.error.issues,
+      })
+    }
+
+    return callback(req as any, res, next)
+  }
+}
+
+export function makeGetOneEndpoint(
+  callback: (
+    req: Request<{id:string}>,
+    res: Response,
+    next: NextFunction,
+  ) => void,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = IdSchema.safeParse(req.params.id)
+
+    if (!result.success) {
+      return res.status(400).send({
+        message: 'Invalid search params',
+        details: result.error.issues,
+      })
+    }
+
+    return callback(req as any, res, next)
+  }
+}
+
+export function makePostEndpoint<Body>(
+  schema: z.ZodType<Body>,
+  callback: (
+    req: Request<any, any, Body>,
+    res: Response,
+    next: NextFunction,
+  ) => void,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body)
+
+    if (!result.success) {
+      return res.status(400).send({
+        message: 'Invalid body',
+        details: result.error.issues,
+      })
+    }
+
+    return callback(req as any, res, next)
+  }
+}
+
+export function makeDeleteEndpoint(
+  callback: (
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction,
+  ) => void,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    
+    const result = IdSchema.safeParse(req.params.id)
+
+    if (!result.success) {
+      return res.status(400).send({
+        message: 'Invalid search params',
+        details: result.error.issues,
+      })
+    }
+
+    return callback(req as any, res, next)
+  }
+}
+
+export function makeUpdateEndpoint<Body>(
+  schema: z.ZodType<Body>,
+  callback: (
+    req: Request<{ id: string }, any, any, Body>,
+    res: Response,
+    next: NextFunction,
+  ) => void,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const IdSchema = z.string().min(1)
+    const result = IdSchema.safeParse(req.params.id)
+
+    if (!result.success) {
+      return res.status(400).send({
+        message: 'Invalid search params',
+        details: result.error.issues,
+      })
+    }
+
+    const bodyResult = schema.safeParse(req.body)
+
+    if (!bodyResult.success) {
+      return res.status(400).send({
+        message: 'Invalid body',
+        details: bodyResult.error.issues,
+      })
+    }
+
+    return callback(req as any, res, next)
+  }
+}
