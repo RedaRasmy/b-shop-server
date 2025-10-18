4b79681c5556d690b828d43897b95846acaf2960
redarasmy <red7mer@gmail.com>
2025-10-18 18:46:51 +0100

    set /me/addresses route

--- src/features/profile/profile.router.ts
+++ src/features/profile/profile.router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { me, updatePassword, updateProfile } from './profile.controller'
 import { cartRouter } from '@profile/cart/cart.router'
+import { addressesRouter } from '@profile/addresses/addresses.router'
 
 const router: Router = Router()
 
@@ -9,5 +10,6 @@ router.patch('/', updateProfile)
 router.patch('/password', updatePassword)
 // Sub Routes
 router.use('/cart', cartRouter)
+router.use('/addresses',addressesRouter)
 
 export const profileRouter = router
