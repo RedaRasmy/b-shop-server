f448e2d13147d929068c5e824e9447d638427063
redarasmy <red7mer@gmail.com>
2025-10-18 14:00:03 +0100

    add update-password endpoint

--- src/features/profile/profile.controller.ts
+++ src/features/profile/profile.controller.ts
@@ -1,5 +1,7 @@
+import { PasswordSchema } from '@auth/auth.validation'
 import { db } from '@db/index'
 import { users } from '@db/schema'
+import { comparePassword, hashPassword } from '@utils/auth'
 import { makePostEndpoint, makeSimpleEndpoint } from '@utils/wrappers'
 import { eq } from 'drizzle-orm'
 import z from 'zod'
@@ -43,6 +45,52 @@ export const updateProfile = makePostEndpoint(
       res.sendStatus(204)
     } catch (err) {
       next(err)
+    }
+  },
+)
+
+export const updatePassword = makePostEndpoint(
+  z.object({
+    oldPassword: PasswordSchema,
+    newPassword: PasswordSchema,
+  }),
+  async (req, res, next) => {
+    const userId = req.user?.id!
+    const { oldPassword, newPassword } = req.body
+    try {
+      // validate old password
+
+      const user = await db.query.users.findFirst({
+        where: (users, { eq }) => eq(users.id, userId),
+      })
+
+      if (!user) {
+        return res.status(401).json({ message: 'Password is incorrect' })
+      }
+
+      const isPasswordCorrect = await comparePassword(
+        oldPassword,
+        user.password,
+      )
+
+      if (!isPasswordCorrect) {
+        return res.status(401).json({ message: 'Password is incorrect' })
+      }
+
+      // update password
+
+      const hashedPassword = await hashPassword(newPassword)
+
+      await db
+        .update(users)
+        .set({
+          password: hashedPassword,
+        })
+        .where(eq(users.id, userId))
+
+      res.sendStatus(204)
+    } catch (error) {
+      next(error)
     }
   },
 )
--- src/features/profile/profile.router.ts
+++ src/features/profile/profile.router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
-import { me, updateProfile } from './profile.controller'
+import { me, updatePassword, updateProfile } from './profile.controller'
 import { cartRouter } from '@profile/cart/cart.router'
 
 const router: Router = Router()
 
 router.get('/', me)
-router.patch('/',updateProfile)
+router.patch('/', updateProfile)
+router.patch('/password', updatePassword)
 // Sub Routes
-router.use('/cart',cartRouter)
+router.use('/cart', cartRouter)
 
 export const profileRouter = router
