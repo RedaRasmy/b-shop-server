b9b1423d4a31844885b22dc121c0017e5f319a34
redarasmy <red7mer@gmail.com>
2025-11-14 21:17:51 +0100

    fix InsertCategorySchema

--- src/features/categories/categories.validation.ts
+++ src/features/categories/categories.validation.ts
@@ -5,7 +5,11 @@ import {
 } from 'drizzle-zod'
 import categories from './categories.table'
 
-export const InsertCategorySchema = createInsertSchema(categories)
+export const InsertCategorySchema = createInsertSchema(categories).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+})
 export const SelectCategorySchema = createSelectSchema(categories)
 export const UpdateCategorySchema = createUpdateSchema(categories).omit({
   id: true,
