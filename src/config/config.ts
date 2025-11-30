10cad42dff0c3207d092bedb2280e305074fb4c5
redarasmy <red7mer@gmail.com>
2025-11-30 20:01:49 +0100

    add 2 mailgun env variables in config

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,6 +15,8 @@ const configSchema = z.object({
   CLOUDINARY_API_SECRET: z.string().min(1),
   CLOUDINARY_URL: z.string().min(1),
   MAILGUN_API_KEY: z.string().min(1),
+  MAILGUN_DOMAIN: z.string().min(1),
+  MAILGUN_FROM: z.string().min(1)
 })
 
 type Config = Partial<z.input<typeof configSchema>>
@@ -30,6 +32,8 @@ const config: Config = {
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_URL: process.env.CLOUDINARY_URL,
   MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
+  MAILGUN_DOMAIN : process.env.MAILGUN_DOMAIN,
+  MAILGUN_FROM : process.env.MAILGUN_FROM 
 }
 
 export default configSchema.parse(config)
